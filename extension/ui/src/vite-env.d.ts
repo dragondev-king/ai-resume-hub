4d6955725fe6505a5d074919d3b2790c3d6ff5f9
danielhubersstorm <daniel.huber.sstorm@gmail.com>
2026-08-31 11:11:02 -0400

    removed for the extension

--- extension/ui/src/vite-env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="chrome" />
-
-declare const __EXT_API_BASE_URL__: string;
-declare const __EXT_SUPABASE_URL__: string;
-declare const __EXT_SUPABASE_ANON_KEY__: string;
